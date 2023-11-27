ead2bbd111ea5fd7856dd84b02dfdb3540947cac
Armond-Guze <Armond.Guze@yahoo.com>
2023-11-26 23:32:27 -0500

    js updates

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
 const searchButton = document.querySelector(".search-btn");
 const cityInput = document.querySelector(".city-input");
 
+
 const ApiKey = "2c4b7c5766e0dd52db1d9b8ea76ac816";
+
+const getWeatherDetails = (cityName, lat, lon) => {
+  const WEATHER_API = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${ApiKey}`;
+
+  fetch(WEATHER_API)
+    .then(response => response.json())
+    .then(data => {
+
+        // Get 1 forecast a day
+        const uniqueForeCastDays = []; 
+      
+      const fiveDaysForecast = data.list.filter(forecast => {
+        const forecastDate = new Date(forecast.dt_txt).getDate();
+        if(!uniqueForeCastDays.includes(forecastDate)) {
+            return uniqueForeCastDays.push(forecastDate);
+        }
+      });
+
+      console.log(fiveDaysForecast);
+      fiveDaysForecast.forEach(weatherItem => {
+        createWeatherCard(weatherItem);
+      })
+
+
+    })
+    .catch(() => {
+      alert("Something went wrong!");
+    });
+};
 
 const getCityCoordinates = () => {
   const cityName = cityInput.value.trim();
@@ -12,6 +42,8 @@ const getCityCoordinates = () => {
     .then(response => response.json())
     .then(data => {
       if (!data.length) return alert("Something went wrong!");
+      const { name, lat, lon } = data[0];
+      getWeatherDetails(name, lat, lon);
     })
     .catch(() => {
       alert("Something went wrong!");
